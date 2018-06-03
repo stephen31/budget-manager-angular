0e2976cb3ca92e4ae2fd264141031e0b6f19ca51
Stephen31 <stephen.baroan@gmail.com>
2018-06-03 22:42:43 +0200

    [Front] setting up routing and start of login

--- frontend/budget-manager/src/app/app.module.ts
+++ frontend/budget-manager/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/budget-manager/src/app/login/login.module.ts
+++ frontend/budget-manager/src/app/login/login.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { LoginFormComponent } from './component/login-form/login-form.component';
+import { LoginPageComponent } from './container/login-page/login-page.component';
+import {MatCardModule} from '@angular/material/card';
+import { LoginRoutingModule } from './login-routing.module';
+
 
 @NgModule({
   imports: [
-    CommonModule
+    SharedModule,
+    MatCardModule,
+    LoginRoutingModule
   ],
-  declarations: []
+  declarations: [LoginFormComponent, LoginPageComponent]
 })
 export class LoginModule { }
