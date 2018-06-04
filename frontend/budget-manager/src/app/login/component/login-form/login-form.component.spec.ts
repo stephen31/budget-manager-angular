b396c6b6707674f2435338fc51a273d448a3c65f
Stephen31 <stephen.baroan@gmail.com>
2018-06-04 22:06:50 +0200

    [Front] improve login form test

--- frontend/budget-manager/src/app/login/component/login-form/login-form.component.spec.ts
+++ frontend/budget-manager/src/app/login/component/login-form/login-form.component.spec.ts
@@ -31,6 +31,18 @@ describe('LoginFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should disable the form when pending is true', () => {
+    spyOn(component.loginForm, 'disable');
+    component.pending = true;
+    expect(component.loginForm.disable).toHaveBeenCalled();
+  });
+
+  it('should enable the form when pending is false', () => {
+    spyOn(component.loginForm, 'enable');
+    component.pending = false;
+    expect(component.loginForm.enable).toHaveBeenCalled();
+  });
+
   it('should create the form', () => {
     component.createForm();
     expect(component.loginForm).toBeDefined();
@@ -38,5 +50,12 @@ describe('LoginFormComponent', () => {
 
   it('should return invalid when form is empty', () => {
     expect(component.loginForm.valid).toBeFalsy();
+  });
+
+  it('should emit a boolean when submit form', () => {
+    component.createForm();
+    spyOn(component.submitted, 'emit');
+    component.submitLogin();
+    expect(component.submitted.emit).toHaveBeenCalledWith(true);
   });
 });
